864abd71c10e6bfeac10c9dcd85486d7d461d490
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-04-17 11:36:43 -0300

    att

--- main.cpp
+++ main.cpp
@@ -1,77 +1,64 @@
-#include <iostream>
+const int Green = 13;
+const int Yellow = 12;
+const int Red = 11;
 
-const int ledVerde = 13;
-const int ledAmarelo = 12;
-const int ledVermelho = 11;
-
-const int ledGrupoB[] = {2, 3, 4, 5, 6, 7, 8};
-const int numLedsGrupoB = 7;
-
+const int GroupB[] = {2, 3, 4, 5, 6, 7, 8};
 const int botao = 10;
 
 void setup() {
-  pinMode(ledVerde, OUTPUT);
-  pinMode(ledAmarelo, OUTPUT);
-  pinMode(ledVermelho, OUTPUT);
-  for (int i = 0; i < numLedsGrupoB; i++) {
-    pinMode(ledGrupoB[i], OUTPUT);
+  pinMode(Green, OUTPUT);
+  pinMode(Yellow, OUTPUT);
+  pinMode(Red, OUTPUT);
+  for (int i = 0; i < 7; i++) {
+    pinMode(GroupB[i], OUTPUT);
   }
   pinMode(botao, INPUT_PULLUP);
 }
 
-void loop() {
-  
+void loop() {  
   while (digitalRead(botao) == HIGH) {
-    digitalWrite(ledVerde, HIGH);
+    digitalWrite(Green, HIGH);
     
      for (int i = 0; i < 5; i++) {
       
-      	for (int j = 0; j < numLedsGrupoB; j++) {
-        digitalWrite(ledGrupoB[j], HIGH);
+      	for (int j = 0; j < 7; j++) {
+        digitalWrite(GroupB[j], HIGH);
         delay(100);
-        digitalWrite(ledGrupoB[j], LOW);
+        digitalWrite(GroupB[j], LOW);
         delay(200);
       }      
     }
     
-    digitalWrite(ledVerde, LOW);
-    digitalWrite(ledAmarelo, HIGH);
-       for (int i = 0; i < 3; i++) {
-      
-      	for (int j = 8; j >=0 ; j--) {
-        digitalWrite(ledGrupoB[j], HIGH);
-        delay(800);
-        //digitalWrite(ledGrupoB[j], LOW);
+    digitalWrite(Green, LOW);
+    digitalWrite(Yellow, HIGH);
+
+    for (int i = 0; i < 3; i++) {
+      for (int j = 8; j >=0 ; j--) {
+      digitalWrite(GroupB[j], HIGH);
+      delay(800);
+      }   
+      for (int j = 0; j < 7; j++) {
+        digitalWrite(GroupB[j], LOW);
       }
-      	digitalWrite(2, LOW);
-        digitalWrite(3, LOW);
-        digitalWrite(4, LOW);
-        digitalWrite(5, LOW);
-        digitalWrite(6, LOW);
-        digitalWrite(7, LOW);
-        digitalWrite(8, LOW);
-     
-    }
+    } 
     
-    digitalWrite(ledAmarelo, LOW);
-    digitalWrite(ledVermelho, HIGH);
+    digitalWrite(Yellow, LOW);
+    digitalWrite(Red, HIGH);
  	
-    for (int k = 0; k < 3; k++) {
-      for (int i = 0; i < 5; i++) { 
-        for (int j = 0; j < numLedsGrupoB; j++) {
-          digitalWrite(ledGrupoB[j], HIGH);
+    for (int R = 0; R < 3; R++) {
+      for (int u = 0; u < 5; u++) { 
+        for (int d = 0; d < 7; d++) {
+          digitalWrite(GroupB[d], HIGH);
         }
         delay(300);
-        for (int j = 0; j < numLedsGrupoB; j++) {
-          digitalWrite(ledGrupoB[j], LOW);
+        for (int j = 0; j < 7; j++) {
+          digitalWrite(GroupB[j], LOW);
         }
         delay(300);
       }
       delay(10000);
     }
     
-    digitalWrite(ledVermelho, LOW);
-    
+    digitalWrite(Red, LOW); 
   }
 }
-
